45db2d05e058fa1d832ac7283697ed7b130fef40
isabellwaas <isabell.waas@hof-university.de>
2024-11-21 12:36:38 +0100

    Improve some datapackages related to cars and add time zone in DateTimeFormat

--- src/main/kotlin/de/uniflitzer/backend/applicationservices/communicators/version1/datapackages/CarpoolDriveOfferCreationDP.kt
+++ src/main/kotlin/de/uniflitzer/backend/applicationservices/communicators/version1/datapackages/CarpoolDriveOfferCreationDP.kt
@@ -3,8 +3,9 @@ package de.uniflitzer.backend.applicationservices.communicators.version1.datapac
 import de.uniflitzer.backend.applicationservices.communicators.version1.valuechecker.UUID
 
 class CarpoolDriveOfferCreationDP private constructor(
+    carIndex: Int,
     freeSeats: Int,
     route: RouteDP,
     plannedDepartureTime: String?,
     @field:UUID val carpoolId: String
-) : DriveOfferCreationDP(freeSeats, route, plannedDepartureTime)
+) : DriveOfferCreationDP(carIndex, freeSeats, route, plannedDepartureTime)
--- src/main/kotlin/de/uniflitzer/backend/applicationservices/communicators/version1/formats/DateTimeFormat.kt
+++ src/main/kotlin/de/uniflitzer/backend/applicationservices/communicators/version1/formats/DateTimeFormat.kt
@@ -1,3 +1,3 @@
 package de.uniflitzer.backend.applicationservices.communicators.version1.formats
 
-const val DateTimeFormat = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}"
+const val DateTimeFormat = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?(?:Z|[+-]\\d{2}:\\d{2})?\$"
