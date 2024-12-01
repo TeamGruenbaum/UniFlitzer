90dac7e5c87f7d24dfafb8961b76ba3c2a63b2df
isabellwaas <isabell.waas@hof-university.de>
2024-12-01 22:41:56 +0100

    Implement DriveRequestsCommunicator

--- src/main/kotlin/de/uniflitzer/backend/repositories/DriveRequestsRepository.kt
+++ src/main/kotlin/de/uniflitzer/backend/repositories/DriveRequestsRepository.kt
@@ -1,7 +1,15 @@
 package de.uniflitzer.backend.repositories
 
 import de.uniflitzer.backend.model.DriveRequest
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.*
 
-interface DriveRequestsRepository: CrudRepository<DriveRequest, UUID> {}
+interface DriveRequestsRepository: JpaRepository<DriveRequest, UUID>
+{
+    @Query("""SELECT d FROM DriveRequest d WHERE (:requestingUserId IS NULL OR d.requestingUser.id = :requestingUserId)""")
+    fun findDriveRequests(pageable: Pageable, @Param("requestingUserId") requestingUserId: UUID? = null): Page<DriveRequest>
+}
